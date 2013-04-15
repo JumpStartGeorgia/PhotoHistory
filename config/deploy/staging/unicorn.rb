265c1bf249d1e79cc9aa4f756b2845a0a2206494
jason <jason.addie@gmail.com>
2013-04-15 09:22:46 +0400

    changing port number

--- config/deploy/staging/unicorn.rb
+++ config/deploy/staging/unicorn.rb
@@ -3,7 +3,7 @@
 ##################################
 root = "/home/photo-history-staging/Photo-History-Staging/current" # path to application current folder
 sock_name = "unicorn_Photo-History-Staging" # must be unique name with no '.'
-port_num = 8106 # must be a unique port number for this application
+port_num = 8108 # must be a unique port number for this application
 tout = 30 # time in seconds before process dies - need a long time for data uploads
 ##################################
 
