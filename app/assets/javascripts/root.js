67395195fb71ea958102e3ff1e1ada2d442bb5e2
vazha-asatiani <vazha.asatiani@jumpstart.ge>
2013-04-16 21:52:31 +0400

    styles updated

--- app/assets/javascripts/root.js
+++ app/assets/javascripts/root.js
@@ -3,21 +3,15 @@ $(function()
   $('.draggable').draggable({ 
     containment: 'parent',
     axis: 'x',
+    cursor: 'col-resize',
     create: function (e, ui)
     {
-      $(this).css('left', $(this).position().left).parent().siblings('.layer2').children('img').width($(this).parent().siblings('img.layer1').width());
+      //create a div on create event in overlay, give overlay overflow hidden
+      $(this).css('left', $(this).parent().width() / 2 - $(this).width() / 2).show().parent().siblings('.layer2').children('img').width($(this).parent().siblings('img.layer1').width());
     },
     drag: function (e, ui)
     {
-      $(this).parent().siblings('.layer2').width($(this).parent().width() - ui.position.left);
-    },
-    start: function ()
-    {
-      $('body').css('cursor', 'col-resize');
-    },
-    stop: function ()
-    {
-      $('body').css('cursor', 'auto');
+      $(this).parent().siblings('.layer2').width($(this).parent().width() - ui.position.left - $(this).width() / 2);
     }
   });
 
