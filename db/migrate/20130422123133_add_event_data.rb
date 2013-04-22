2f1e647f8f485eeadd1cffdf5a0d77dba0585f6d
jason <jason.addie@gmail.com>
2013-04-22 19:04:05 +0400

    moved migration stack order due to obj name change

--- db/migrate/20130422123133_add_event_data.rb
+++ db/migrate/20130422123133_add_event_data.rb
@@ -1,16 +1,16 @@
 # encoding: UTF-8
 class AddEventData < ActiveRecord::Migration
   def up
-    l = Location.create(:type_id => Location::TYPES[:event])
-    l.location_translations.create(:locale => 'ka', :name => 'საბჭოთა ოკუპაცია 1921-1989')
-    l.location_translations.create(:locale => 'en', :name => 'Soviet Occupation 1921-1991')
+    l = Category.create(:type_id => Category::TYPES[:event])
+    l.category_translations.create(:locale => 'ka', :name => 'საბჭოთა ოკუპაცია 1921-1989')
+    l.category_translations.create(:locale => 'en', :name => 'Soviet Occupation 1921-1991')
 
-    l = Location.create(:type_id => Location::TYPES[:event])
-    l.location_translations.create(:locale => 'ka', :name => 'სამოქალაქო ომი 1991-1992')
-    l.location_translations.create(:locale => 'en', :name => 'Civil War 1991-1992')
+    l = Category.create(:type_id => Category::TYPES[:event])
+    l.category_translations.create(:locale => 'ka', :name => 'სამოქალაქო ომი 1991-1992')
+    l.category_translations.create(:locale => 'en', :name => 'Civil War 1991-1992')
   end
 
   def down
-    Location.where(:type_id => Location::TYPES[:event]).destroy_all
+    Category.where(:type_id => Category::TYPES[:event]).destroy_all
   end
 end
